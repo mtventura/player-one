8a6c45346fe00468af82035ee85d57e28fd2b73e
mtventura <mventura@gauss-sistemas.com.ar>
2021-09-07 22:48:17 -0300

    Elimino Card y Card Style. Creo Item, Item List. Agrego el useEffect para los pasar los items al ItemList.

---
+++ src/components/Item/Item.js
@@ -0,0 +1,17 @@
+import { StyledItemCount} from '../ItemCount/ItemCount.Style'
+
+const options = {style: 'currency', currency:'ARS'}
+const numberFormat = new Intl.NumberFormat('es-AR', options)
+const Item = ({className, item}) =>{
+
+    return(
+        <div className={className}>
+            <img alt="" src={item.pictureUrl}/>
+            <h3>{numberFormat.format(item.price)}</h3>
+            <h3>{item.title}</h3>
+            <StyledItemCount stock={item.stock}/>
+        </div>
+    )
+}
+
+export default Item; 
